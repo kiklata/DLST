2a4b70ae132a0dec5012312071cc8f6e7a6eeda4
kiklata <929675410@qq.com>
2022-06-03 14:31:47 +0800

    upload

--- code/feature_calculate.R
+++ code/feature_calculate.R
@@ -14,7 +14,7 @@ model = load_model_tf(filepath = 'D:/bioinfo/DLSPseq/data/V2_2_normal_epoch16')
 #summary(model)
 model_feature = keras_model(inputs = model$input,outputs = get_layer(model, 'global_average_pooling2d_1')$output)
 
-path = 'D:/bioinfo/DLSPseq/data/norm_img/test10000'
+path = 'D:/bioinfo/DLSPseq/data/norm_img/20220606'
 # path = paste0('D:/bioinfo/DLSPseq/data/norm_img/', as.character(args[1])) # path to parent dir of pics
 sample_list = list.dirs(path, recursive = F)
 sample_step = nchar(path) + 2
