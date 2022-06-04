9c4036c414b288788f7cc81107fc91788d586544
kiklata <929675410@qq.com>
2022-06-04 09:02:44 +0800

    Update feature_calculate.R

--- code/feature_calculate.R
+++ code/feature_calculate.R
@@ -6,6 +6,7 @@ rm(list = ls())
 library(keras)
 library(tfhub)
 library(data.table)
+library(progress)
 tensorflow::set_random_seed(42,disable_gpu = T)
 # args = commandArgs(T)
 
@@ -14,7 +15,7 @@ model = load_model_tf(filepath = 'D:/bioinfo/DLSPseq/data/V2_2_normal_epoch16')
 #summary(model)
 model_feature = keras_model(inputs = model$input,outputs = get_layer(model, 'global_average_pooling2d_1')$output)
 
-path = 'D:/bioinfo/DLSPseq/data/norm_img/20220606'
+path = 'D:/bioinfo/DLSPseq/data/norm_img/20220608'
 # path = paste0('D:/bioinfo/DLSPseq/data/norm_img/', as.character(args[1])) # path to parent dir of pics
 sample_list = list.dirs(path, recursive = F)
 sample_step = nchar(path) + 2
@@ -22,7 +23,12 @@ img_step = nchar(sample_list[1]) + 2
 for (i in 1:length(sample_list)) {
   img_list = list.files(sample_list[i], full.names = T)
   print(substring(sample_list[i],sample_step))
-  pb = utils::txtProgressBar(max = length(img_list),style = 3)
+
+  pb = progress_bar$new(
+    format = '[:bar] :current/:total in :elapsed eta: :eta',
+    total = length(img_list), clear = FALSE, width = 80
+  )
+  
   img_features = list()
   start_time = Sys.time()
   for (k in 1:length(img_list)) {
@@ -32,9 +38,10 @@ for (i in 1:length(sample_list)) {
                        imagenet_preprocess_input(array_reshape(x, c(1, dim(x)))))
     img_features[[k]] = features
     names(img_features)[[k]] = substring(img_list[k],img_step)
-    utils::setTxtProgressBar(pb,k)
+
+    pb$tick()
+    
   }
-  close(pb)
   img_features = as.data.frame(t(rbindlist(list(img_features))))
   save(img_features,file = paste0(sample_list[i],'_train_features.Rdata'))
   end_time = Sys.time()
