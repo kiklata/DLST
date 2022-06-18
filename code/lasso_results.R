20323a7a5cc2e3e0d45a2ee9602dc2e165de9c5a
kiklata <929675410@qq.com>
2022-06-18 09:45:44 +0800

    Update lasso_results.R

--- code/lasso_results.R
+++ code/lasso_results.R
@@ -4,12 +4,13 @@ library(doMC)
 registerDoMC(cores = 8)
 library(dplyr)
 library(progress)
+# library(mpath)
 
 sample = 'BT23567'
 sample_feature = filter(four_feature,substring(rownames(four_feature),1,7)==sample)
 sample_ST = filter(four_ST,substring(rownames(four_ST),1,7)==sample)
 
-train_test_ratio = 1
+train_test_ratio = 0.8
 sample_size = nrow(sample_feature)
 train_size = round(sample_size*train_test_ratio)
 
@@ -18,7 +19,7 @@ train_x = as.matrix(sample_feature[1:train_size,])
 cor_list = list()
 
 sample_ST_zero_filter = t(sample_ST)
-sample_ST_zero_filter = sample_ST_zero_filter[rowMeans(sample_ST_zero_filter == 0)<0.8,]
+sample_ST_zero_filter = sample_ST_zero_filter[rowMeans(sample_ST_zero_filter == 0)<0.9,]
 sample_ST_zero_filter = t(sample_ST_zero_filter)
 
 pb = progress_bar$new(
@@ -29,11 +30,13 @@ pb = progress_bar$new(
 for (i in 1:ncol(sample_ST_zero_filter)) {
 
   pb$tick()
-  
+  i =13
   target_gene = colnames(sample_ST_zero_filter)[i]
   train_y = as.matrix(sample_ST_zero_filter[,target_gene][1:train_size])
   
-  fit = cv.glmnet(train_x,train_y,family ="gaussian",alpha = 1,nfolds = 10, parallel = TRUE)
+  fit = cv.glmnet(train_x,train_y,family = 'poisson'(link = 'log'),alpha = 1,nfolds = 10, parallel = TRUE)
+#  fit = cv.glmregNB(train_y~train_x)
+  
   best = fit$lambda.1se
   train_predicted = predict(fit,s=best,newx=train_x)
   pred = cor.test(train_predicted,train_y)
@@ -42,11 +45,11 @@ for (i in 1:ncol(sample_ST_zero_filter)) {
   
 }
 
-#test_x = as.matrix(sample_feature[train_size:sample_size,])
-#test_y = as.matrix(sample_ST[,target_gene][train_size:sample_size])
+# test_x = as.matrix(sample_feature[train_size:sample_size,])
+# test_y = as.matrix(sample_ST[,target_gene][train_size:sample_size])
 
 cor_results =  rbindlist(cor_list)
 cor_results = na.omit(cor_results)
 cor_results$pvalue_adj = p.adjust(cor_results$pvalue,method = 'BH')
 
-saveRDS(cor_results,file = paste0(sample,'lasso_cor_results'))
+saveRDS(cor_results,file = paste0(sample,'_lasso_cor_results.rds'))
